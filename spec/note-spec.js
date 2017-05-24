4a0db844b8814a63ea4fd33fea2821db3c5371b2
Sophie Mann <sklm@live.co.uk>
2017-05-24 13:06:57 +0100

    adds ID to note model

--- spec/note-spec.js
+++ spec/note-spec.js
@@ -1,6 +1,5 @@
 var note = new Note("My favourite language is JavaScript");
 
-
 function testNoteInitializesWithText(){
   assert.isTrue(note._text === "My favourite language is JavaScript");
 }
@@ -12,3 +11,16 @@ function testNoteTextCanBeRetrieved(){
 }
 
 testNoteTextCanBeRetrieved();
+
+function testNoteHasID(){
+  assert.isEq(note.getID(), 0);
+}
+
+testNoteHasID();
+
+function testNoteIDIncrements(){
+  var newnote = new Note("String");
+  assert.isEq(newnote.getID(), 1);
+}
+
+testNoteIDIncrements();
--- src/note.js
+++ src/note.js
@@ -1,13 +1,22 @@
 'use strict';
 
 (function(exports) {
+
+  var NOTE_COUNT = 0;
+
   function Note(message) {
     this._text = message;
+    this._id = NOTE_COUNT;
+    NOTE_COUNT ++;
   }
 
   Note.prototype.getText = function(){
     return this._text;
   };
+
+  Note.prototype.getID = function() {
+    return this._id;
+  }
 
   exports.Note = Note;
 })(this);
