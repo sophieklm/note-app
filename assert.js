28482112c261dd21a13c5add7fd5495dea9cc9b1
Sophie Mann <sklm@live.co.uk>
2017-05-24 08:28:56 +0100

    adds assertion matchers

---
+++ assert.js
@@ -0,0 +1,23 @@
+var assert = {
+
+  isTrue: function(assertionToCheck) {
+    if (!assertionToCheck) {
+      throw new Error("Assertion failed: " + assertionToCheck + " is not truthy");
+    }
+    console.log('isTrue assertion passed');
+  },
+
+  isFalse: function(assertionToCheck) {
+    if (!!assertionToCheck) {
+      throw new Error("Assertion failed: " + assertionToCheck + " is not falsy");
+    }
+    console.log('isFalse assertion passed');
+  },
+
+  isEq: function(assertionToCheck, otherAssertion) {
+    if(assertionToCheck !== otherAssertion) {
+      throw new Error("Assertion failed: " + assertionToCheck + " does not equal " + otherAssertion);
+    }
+    console.log('isEq assertion passed');
+  },
+};
